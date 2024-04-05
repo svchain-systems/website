13e8a5f1fb31a2dbee384857bf44a3f0c913658f
Adimac93 <adimac93@gmail.com>
2024-04-05 14:04:47 +0200

    pitch day content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sustainable value chain';
-export const SITE_DESCRIPTION = 'Work in progress';
+export const SITE_TITLE = 'SVC systems';
+export const SITE_DESCRIPTION = 'Sustainable value chain systems';
